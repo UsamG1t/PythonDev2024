dae2cf19fad6ed8ecdc88ca6df556acbeeb90c20
Stephen <UsamUNX>
2024-02-25 19:35:49 +0300

    Add handler for character's list

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,7 +3,10 @@ from cowsay import cowsay, list_cows, Option
 
 parser = argparse.ArgumentParser(
                     description = "cowsay-alike program",
-                    epilog = "If u're tired, go to bed, dudes. Try to understand this program later")
+                    epilog = """
+                    If u're tired, go to bed, dudes.
+                    Try to understand this program later
+                    """)
 
 parser.add_argument('message', metavar = 'text', type = str, nargs = '?',
                     action = 'store',
@@ -12,6 +15,8 @@ parser.add_argument('-c', '--character', type = str, nargs = '?', action = 'stor
                     default = 'default', help = 'The name of the cow')
 parser.add_argument('-e', '--eyes', type = str, nargs = '?', action = 'store',
                     default = Option.eyes, help = 'cows eye-string')
+parser.add_argument('-l', action = 'store_true', 
+                    help = 'show list of characters')
 parser.add_argument('-f', '--file', type = argparse.FileType('W'), nargs = '?',
                     help = 'custom cow file')
 parser.add_argument('-T', '--tongue', type = str, nargs = '?', action = 'store',
@@ -21,4 +26,8 @@ parser.add_argument('-W', '--width', type = int, nargs = '?',  default = 40,
 
 args = parser.parse_args()
 
-print(cowsay(args.message, cow = args.character, eyes = args.eyes, tongue = args.tongue, width = args.width))
+if args.l:
+    print(sorted(list_cows()))
+else:
+    print( cowsay(args.message, cow = args.character, eyes = args.eyes, 
+                                tongue = args.tongue, width = args.width))
